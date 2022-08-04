cfb231dc20af8f2c8043b6e1ddef3426f33d8a15
kimvnhung <kimvnhung@gmail.com>
2022-08-04 16:36:16 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ refresh();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  initObject.push({positions : "[]",orders:"[]"})
+  refresh()
   res.render('index',initObject);
 });
 
@@ -18,8 +18,8 @@ function refresh(){
   updateTotalLost();
   updateTopPnl();
   updateTopRoe();
-  // updateOrders();
-  // updatePositions();
+  updateOrders();
+  updatePositions();
 }
 
 var host = "https://binanceapi-go.herokuapp.com"
@@ -28,42 +28,30 @@ function updateBalance() {
   //update balance
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getBalance";
-  xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({totalBalance : rs.balance})
-    }
-  };
+  xmlhttp.open("GET", url, false);
   xmlhttp.send();
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({totalBalance : rs.balance})
 }
 
 function updateTotalGain() {
   //update totalGain
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getTotalGain"
-  xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({totalGain : rs.totalGain})
-    }
-  };
+  xmlhttp.open("GET", url, false);
   xmlhttp.send();
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({totalGain : rs.totalGain})
 }
 
 function updateTotalLost() {
   //update totalLost
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getTotalLost"
-  xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({totalLost : rs.totalLost})
-    }
-  };
+  xmlhttp.open("GET", url, false);
   xmlhttp.send();
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({totalLost : rs.totalLost})
 }
 
 function updateTopPnl(){
@@ -71,56 +59,40 @@ function updateTopPnl(){
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getTopPnl"
   xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({topPnl : rs.topPnl})
-    }
-  };
   xmlhttp.send();
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({topPnl : rs.topPnl})
 }
 
 function updateTopRoe() {
   //update topRoe
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getTopRoe"
-  xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({topRoe : rs.topRoe})
-    }
-  };
+  xmlhttp.open("GET", url, false);
   xmlhttp.send();
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({topRoe : rs.topRoe})
 }
 
 function updatePositions() {
   //update positions
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getPositions"
-  xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({positions : rs.positions})
-    }
-  };
+  xmlhttp.open("GET", url, false);
   xmlhttp.send();
-
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({positions : rs.positions})
 }
 
 function updateOrders() {
   //update orders
   var xmlhttp = new XMLHttpRequest();
   var url = host+"/v1/getOrders"
-  xmlhttp.open("GET", url, true);
-  xmlhttp.onreadystatechange = function(response) {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var rs = JSON.parse(xmlhttp.responseText)
-      initObject.push({orders : rs.orders})
-    }
-  };
+  xmlhttp.open("GET", url, false);
   xmlhttp.send();
+
+  var rs = JSON.parse(xmlhttp.responseText)
+  initObject.push({orders : rs.orders})
 }
 
 // setInterval(updateView,10000)
